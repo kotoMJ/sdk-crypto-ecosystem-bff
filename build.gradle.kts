15058d431d4fa2ce8d057b792ad237cc4d7d5339
Michal Jenicek <michal.jenicek@strv.com>
2025-12-26 21:49:06 +0100

    Add spotless and detekt plugins.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     alias(libs.plugins.versions)
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.detekt)
 }
 
 group = "cz.kotox.crypto.sdk"
@@ -19,4 +21,44 @@ dependencies {
     implementation(libs.ktor.server.config.yaml)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    // Tell the plugin to only check for stable Gradle versions
+    gradleReleaseChannel = "current"
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+// 1. Configure Spotless (The Formatter)
+spotless {
+    kotlin {
+        target("**/*.kt")
+        // Use the official Kotlin style guide
+        ktlint()
+        // Or strictly follow standard .editorconfig
+        // ktlint().setEditorConfigPath("$projectDir/.editorconfig")
+
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint()
+    }
+}
+
+// 2. Configure Detekt (The Linter)
+detekt {
+    buildUponDefaultConfig = true // Use default rules + your overrides
+    allRules = false // true if you want to be very strict
+    // config.setFrom(files("$projectDir/config/detekt/detekt.yml")) // Your config file
 }
--- src/main/kotlin/cz/kotox/crypto/sdk/Application.kt
+++ src/main/kotlin/cz/kotox/crypto/sdk/Application.kt
@@ -1,9 +1,10 @@
 package cz.kotox.crypto.sdk
 
-import io.ktor.server.application.*
+import io.ktor.server.application.Application
+import io.ktor.server.netty.EngineMain
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    EngineMain.main(args)
 }
 
 fun Application.module() {
